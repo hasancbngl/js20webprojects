fb5eb249e6b8ee4f111b1691ced4ab31d210752b
Hasan Çobanoğlu <hasan.cobanoglu1@outlook.com.tr>
2021-03-08 15:13:27 +0300

    refactoring

--- 13-spock-rock-game/script.js
+++ 13-spock-rock-game/script.js
@@ -4,24 +4,12 @@ const playerChoiceEl = document.getElementById('playerChoice');
 const computerChoiceEl = document.getElementById('computerChoice');
 const resultText = document.getElementById('resultText');
 
-const playerRock = document.getElementById('playerRock');
-const playerPaper = document.getElementById('playerPaper');
-const playerScissors = document.getElementById('playerScissors');
-const playerLizard = document.getElementById('playerLizard');
-const playerSpock = document.getElementById('playerSpock');
-const computerRock = document.getElementById('computerRock');
-const computerPaper = document.getElementById('computerPaper');
-const computerScissors = document.getElementById('computerScissors');
-const computerLizard = document.getElementById('computerLizard');
-const computerSpock = document.getElementById('computerSpock');
-
 const allGameIcons = document.querySelectorAll('.far');
-
-const resetSelected = () => {
-  allGameIcons.forEach((icon) => {
-    icon.classList.remove('selected');
-  });
-};
+let computerChoice = '';
+let computerChoiceNumber = 0;
+const choicesEl = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+const computerChoicesAr = ['computerRock', 'computerPaper', 'computerScissors', 'computerLizard', 'computerSpock'];
+const playerChoicesAr = ['playerRock', 'playerPaper', 'playerScissors', 'playerLizard', 'playerSpock'];
 
 const choices = {
   rock: { name: 'Rock', defeats: ['scissors', 'lizard'] },
@@ -31,35 +19,36 @@ const choices = {
   spock: { name: 'Spock', defeats: ['scissors', 'rock'] },
 };
 
+const resetSelected = () => {
+  allGameIcons.forEach((icon) => {
+    icon.classList.remove('selected');
+  });
+};
+
+const randomComputerChoice = () => {
+  computerChoiceNumber = Math.floor(Math.random() *5);
+  computerChoice =  choicesEl[computerChoiceNumber];
+  computerChoiceEl.textContent = ' ---' + computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1);
+  let element = document.getElementById(computerChoicesAr[computerChoiceNumber]);
+  element.classList.add('selected');
+};
+
+//call func to process turn
+const checkResult = () => {
+  resetSelected();
+  randomComputerChoice();
+};
+
 //pass player selection and styling icons
 const select = playerChoice => {
-  resetSelected();
-
-  let chosen = '';
-  //add selected styling
-  switch(playerChoice) {
-    case 'rock':
-      playerRock.classList.add('selected');
-      chosen = 'Rock';
-      break;
-    case 'paper':
-      playerPaper.classList.add('selected');
-      chosen = 'Paper';
-      break;
-    case 'scissors':
-      playerScissors.classList.add('selected');
-      chosen = 'Scissors';
-      break;
-    case 'lizard':
-      playerLizard.classList.add('selected');
-      chosen = 'Lizard';
-      break;
-      case 'spock':
-      playerSpock.classList.add('selected');
-      chosen = 'Spock';
-      break;
-      default:
-        break;
-  }
-  playerChoiceEl.textContent = '---' + chosen;
+  checkResult();
+  let index;
+  choicesEl.forEach((el)=>{
+    if(el===playerChoice) {
+      index =  choicesEl.indexOf(el);
+      playerChoiceEl.textContent = ' ---' + el.charAt(0).toUpperCase() + el.slice(1);
+      let element = document.getElementById(playerChoicesAr[index]);
+      element.classList.add('selected');
+    }
+  });
 };
