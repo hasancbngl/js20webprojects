e5c6bc0580089deac7139ead89e370efb0ca1717
Hasan Çobanoğlu <hasan.cobanoglu1@outlook.com.tr>
2021-03-27 19:00:33 +0300

    calculator complete

--- 17-calculator/script.js
+++ 17-calculator/script.js
@@ -2,26 +2,65 @@ const calculatorDisplay = document.querySelector('h1');
 const inputButtons = document.querySelectorAll('button');
 const clearButton = document.getElementById('clear-btn');
 
-const sendNumberValue = number => {
-    const displayValue = calculatorDisplay.textContent;
-    calculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number;
+let firstValue = 0;
+let operatorValue = '';
+let awaitingNextValue = false;
+//calculate values 
+const calculate = {
+    '/': (firstNumber, secondNumber) => firstNumber / secondNumber,
+    '*': (firstNumber, secondNumber) => firstNumber * secondNumber,
+    '+': (firstNumber, secondNumber) => firstNumber + secondNumber,
+    '-': (firstNumber, secondNumber) => firstNumber - secondNumber,
+    '=': (firstNumber, secondNumber) => secondNumber,
 };
 
+const sendNumberValue = number => {
+    if(awaitingNextValue) {
+        calculatorDisplay.textContent = number;
+        awaitingNextValue = false;
+    } else {
+    const displayValue = calculatorDisplay.textContent;
+    calculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number;
+    }
+};
+
+//reset values
 const clearAll = () => {
     calculatorDisplay.textContent = 0;
+    firstValue = 0;
+    operatorValue = '';
+    awaitingNextValue = false;
 };
 
 const addDecimal = () => {
+    if(awaitingNextValue) return;
     if(!calculatorDisplay.textContent.includes('.')) {
         calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
     }
+};
+const addOperator = (operator) => {
+    const currentValue = Number(calculatorDisplay.textContent);
+    if(operatorValue && awaitingNextValue) {
+        operatorValue = operator;
+        return;
+    }
+    if(!firstValue) {
+        firstValue = currentValue;
+    } else {
+        const calculation = calculate[operatorValue](firstValue, currentValue);
+        calculatorDisplay.textContent = calculation;
+        firstValue = calculation;
+    }
+    //ready for next value, store operator 
+    awaitingNextValue = true;
+    operatorValue = operator;
 };
 
 inputButtons.forEach((el) => {
     if(el.classList.length === 0) {
     el.addEventListener('click', ()=>sendNumberValue(el.value));
     } else if(el.classList.contains('operator')) {
-        el.addEventListener('click', ()=>sendNumberValue(el.value));
+        el.addEventListener('click', ()=>addOperator(el.value));
     } else if(el.classList.contains('decimal')) {
         el.addEventListener('click', addDecimal);
     } else {
